e1aa42e87be9622b3202db54f2368720be218aae
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2023-10-27 21:39:44 +0900

    Update and rename ch2_ex7.c to LogicalOperator.c

---
+++ ch2_수식과연산자/LogicalOperator.c
@@ -0,0 +1,20 @@
+// 논리연산자 이해하기
+#include <stdio.h>
+
+int main(void)
+{
+	int num1, num2;		
+
+	printf("두 개의 정수를 입력하시오 : "); 
+	scanf_s("%d %d", &num1, &num2);			
+
+	printf("%d && %d 의 결과값 : %d\n", num1, num2, num1 && num2);	
+	// num1과 num2의 and 값을 출력한다. 두 값 중에서 0이 없으면 1을 출력한다.
+	
+	printf("%d || %d의 결과값 : %d\n", num1, num2, num1 || num2);
+	// num1과 num2의 or 값을 출력한다. 두 값 중에서 하나라도 0이 아니라면 1을 출력한다.
+	
+	printf("!%d의 결과값 : %d\n", num1, !num1);
+	// num1이 아닌 값을 출력한다. num1이 0이 아닌 값이라면, 0을 출력하고 num1이 0이라면 1을 출력한다.
+	return 0;
+}
