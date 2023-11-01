46793991817f6845f2cd900b913b6f609bf3f131
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2023-11-01 21:43:02 +0900

    Update and rename ch4_lab3_49p.c to ch4_GuessNumberRandom.c

---
+++ ch4_반복문/ch4_GuessNumberRandom.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	srand((unsigned)time(NULL));			// 난수 발생기 시드 
+	int answer = rand() % 100;				
+	int guess;								
+	int tryy = 0;							
+
+	do {
+		printf("정답을 추측하시오 : ");		
+		scanf_s("%d", &guess);				
+		tryy++;
+		if (guess < answer)		// 추측한 숫자가 정답보다 작다면 LOW 출력
+			printf("LOW\n");
+		if (guess > answer)		// 추측한 숫자가 정답보다 크다면 HIGH 출력
+			printf("HIGH\n");
+	} while (guess != answer);	// 추측한 숫자가 정답과 일치하면 반복종료
+	printf(" 축하합니다. 시도횟수 = %d", tryy);
+	return 0;
+}
